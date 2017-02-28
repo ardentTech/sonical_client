a37aa122c37ac2b717b31f4198337bcc64de726a
Jonathan D. Baker <jbaker@comverge.com>
2017-02-28 10:49:20 -0700

    http requests working

--- src/Main.elm
+++ src/Main.elm
@@ -1,30 +1,66 @@
 module Main exposing (main)
 
 import Html exposing (Html, div, text)
+import Http
+import Json.Decode exposing (Decoder, at, list, string)
+import Json.Decode.Pipeline exposing (decode, required)
 
 
 type alias Driver = { model : String }
 type alias Model = { drivers: List Driver }
-type Msg = NoOp
+type Msg = Fail Http.Error | FetchDriversDone (Result Http.Error (List Driver)) | NoOp
 
 
-init : ( Model, Cmd Msg )
-init = ( { drivers = [] }, Cmd.none )
-
+-- View.elm
 
 view : Model -> Html Msg
 view model = div [] [ text "Howdy!" ]
 
 
+-- Update.elm
+
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
+      Fail _ ->
+        ( model, Cmd.none )
+      FetchDriversDone (Ok drivers) ->
+        ({ model | drivers = drivers }, Cmd.none )
+      FetchDriversDone (Err _) ->
+        ( model, Cmd.none )
       NoOp ->
         ( model, Cmd.none )
 
 
+-- Subscriptions.elm
+
 subscriptions : Model -> Sub Msg
 subscriptions model = Sub.none
+
+
+-- Decoders.elm
+
+driverDecoder : Decoder Driver
+driverDecoder = decode Driver
+  |> required "model" string
+
+
+driversDecoder : Decoder (List Driver)
+driversDecoder =
+  at ["results"] (list driverDecoder)
+
+
+-- Commands.elm
+
+fetchDrivers : Cmd Msg
+fetchDrivers =
+  Http.send FetchDriversDone <| Http.get "http://localhost:8001/v1/drivers/" driversDecoder
+
+
+-- Main.elm
+
+init : ( Model, Cmd Msg )
+init = ( { drivers = [] }, fetchDrivers )
 
 
 main : Program Never Model Msg
