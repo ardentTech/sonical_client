baf3c20e332d7980691aa171f62ebbf68ae6bb2d
Jonathan D. Baker <jbaker@comverge.com>
2017-03-07 10:44:12 -0700

    pagination styles and info

--- src/Views.elm
+++ src/Views.elm
@@ -3,7 +3,7 @@ module Views exposing (view)
 import Html exposing (Html, button, div, table, tbody, td, text, thead, th, tr)
 import Html.Attributes exposing (class, disabled, id, title)
 import Html.Events exposing (onClick)
-import List exposing (map)
+import List exposing (map, length)
 
 import Messages exposing (Msg (NextPageClicked, PrevPageClicked))
 import Models exposing (Driver, FrequencyResponse, Model)
@@ -13,12 +13,19 @@ import Units exposing (decibels, hertz, ohms, watts)
 
 view : Model -> Html Msg
 view model = 
-  div [ id "drivers" ] [
-    table [ class "table table-responsive table-sm table-striped" ] [
-      thead [] [ tableHeaderRow ],
-      tbody [] (map tableBodyRow model.drivers)
+  div [ class "row" ] [
+    div [ class "col-12", id "drivers" ] [
+      table [ class "table table-responsive table-sm table-striped" ] [
+        thead [] [ tableHeaderRow ],
+        tbody [] (map tableBodyRow model.drivers)
+      ]
     ],
-    pagination model
+    div [ class "col-6", id "pagination-info" ] [
+      paginationInfo model
+    ],
+    div [ class "col-6", id "pagination-controls" ] [
+      paginationControls model
+    ]
   ]
 
 
@@ -70,19 +77,28 @@ page endpoint msg txt =
         Just v -> False
   in
     button [
-      class "btn btn-secondary",
+      class "btn btn-secondary btn-sm",
       disabled disabledVal,
       onClick msg
     ] [ text txt ]
 
 
-pagination : Model -> Html Msg
-pagination model =
+paginationControls : Model -> Html Msg
+paginationControls model =
   let
     prev = prevPage model
     next = nextPage model
   in
-    div [ class "btn-group", id "pagination" ] [ prev, next ]
+    div [ class "btn-group float-right", id "pagination" ] [ prev, next ]
+
+
+paginationInfo : Model -> Html Msg
+paginationInfo model =
+  let
+    currentCount = (toString <| length model.drivers)
+    totalCount = (toString model.driversCount)
+  in
+    text <| "Showing " ++ currentCount ++ " of " ++ totalCount ++ " items"
 
 
 power : Maybe number -> String
