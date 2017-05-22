c30cb25715395e34fde29023d013cb1fea2c8769
Jonathan D. Baker <jbaker@comverge.com>
2017-05-22 14:42:49 -0600

    string param to int

--- src/Router.elm
+++ src/Router.elm
@@ -1,15 +1,15 @@
 module Router exposing (..)
 
-import UrlParser exposing ((</>), (<?>), Parser, int, map, oneOf, s, stringParam, top)
+import UrlParser exposing ((</>), (<?>), Parser, int, map, oneOf, s, intParam, top)
 
 
-type Route = DriverList (Maybe String) (Maybe String) | DriverDetail Int | ManufacturerList
+type Route = DriverList (Maybe Int) (Maybe Int) | DriverDetail Int | ManufacturerList
 
 
 route : Parser (Route -> a) a
 route =
   oneOf [
     map (DriverList Nothing Nothing) top,
-    map DriverList (s "drivers" <?> stringParam "limit" <?> stringParam "offset"),
+    map DriverList (s "drivers" <?> intParam "limit" <?> intParam "offset"),
     map DriverDetail (s "drivers" </> int),
     map ManufacturerList (s "manufacturers")]
